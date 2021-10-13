9ff23cc7e3fd468833138f64787529052d818e49
iferrer20 <iferreriestacio@gmail.com>
2021-10-13 17:03:25 +0200

    Designing frontend

--- frontend-angular/src/app/user/user-routing.module.ts
+++ frontend-angular/src/app/user/user-routing.module.ts
@@ -1,8 +1,12 @@
+import { SigninComponent } from './signin.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user.component';
 
-const routes: Routes = [{ path: '', component: UserComponent }];
+const routes: Routes = [
+  { path: '', component: UserComponent },
+  { path: 'signin', component: SigninComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- frontend-angular/src/app/user/user.module.ts
+++ frontend-angular/src/app/user/user.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
+import { SigninComponent } from './signin.component';
+import { SignupComponent } from './signup.component';
 
 
 @NgModule({
   declarations: [
-    UserComponent
+    UserComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     CommonModule,
